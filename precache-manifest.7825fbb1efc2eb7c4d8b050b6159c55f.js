aab30fbc0d2dcd75d9bbf7619e0a442aa94abee3
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2019-10-18 23:25:31 +0300

    Updates

---
+++ precache-manifest.7825fbb1efc2eb7c4d8b050b6159c55f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac844c603d17115e9b7fe96615db295",
+    "url": "/ReactLearning/index.html"
+  },
+  {
+    "revision": "6500fe5eb01cdb40e1d0",
+    "url": "/ReactLearning/static/css/main.472d3243.chunk.css"
+  },
+  {
+    "revision": "c397cf080205832e7969",
+    "url": "/ReactLearning/static/js/2.328a7d6e.chunk.js"
+  },
+  {
+    "revision": "6500fe5eb01cdb40e1d0",
+    "url": "/ReactLearning/static/js/main.a990d635.chunk.js"
+  },
+  {
+    "revision": "935a8490eeb6ae761c28",
+    "url": "/ReactLearning/static/js/runtime~main.dc71acf1.js"
+  },
+  {
+    "revision": "3f18d3111815cd99c3ac1f22c2edb60d",
+    "url": "/ReactLearning/static/media/friendsImg.3f18d311.jpg"
+  }
+]);
